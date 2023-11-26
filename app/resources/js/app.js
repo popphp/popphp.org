34702083ff3141f13f9799f5f3f190f6d527ebab
Nick Sagona <nick@nolainteractive.com>
2023-11-26 17:55:23 -0600

    Work on v5 website

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,11 +1,18 @@
 import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
+import hljs from 'highlight.js/lib/core'
+import json from 'highlight.js/lib/languages/json'
+import bash from 'highlight.js/lib/languages/bash'
 import { copyCode } from './modules/main'
 
 window.Alpine = Alpine
 
 Alpine.plugin(persist)
 
+Alpine.store('navigationOpen', false)
 Alpine.data('copyCode', copyCode)
 Alpine.start()
 
+hljs.registerLanguage('json', json)
+hljs.registerLanguage('bash', bash)
+hljs.highlightAll()
