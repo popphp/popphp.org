57f0bbe55105325b8b3324fa0691f730744d8a90
Nick Sagona <nick@nolainteractive.com>
2023-11-27 17:49:55 -0600

    Work on new layout

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,6 +1,7 @@
 import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
 import hljs from 'highlight.js/lib/core'
+import php from 'highlight.js/lib/languages/php'
 import json from 'highlight.js/lib/languages/json'
 import bash from 'highlight.js/lib/languages/bash'
 import { copyCode } from './modules/main'
@@ -13,6 +14,7 @@ Alpine.store('navigationOpen', false)
 Alpine.data('copyCode', copyCode)
 Alpine.start()
 
+hljs.registerLanguage('php', php)
 hljs.registerLanguage('json', json)
 hljs.registerLanguage('bash', bash)
 hljs.highlightAll()
