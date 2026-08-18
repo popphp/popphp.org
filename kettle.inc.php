e45a4f5dc658c08876510cfb004afd0c55dd77dd
Nick Sagona <nick@noladev.com>
2026-08-17 21:38:37 -0500

    Install Pop, add kettle

---
+++ kettle.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Kettle Include File
+ *
+ * Allows you to hook into the Kettle helper application by bootstrapping
+ * any additional runtime requirements, configurations or routes.
+ *
+ * The main $autoloader and $config objects are accessible in this file.
+ */
+
